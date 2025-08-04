1703645eefe04763ea06ab98c3862e7f03bf556d
QuinnAhoJHUAPL <quinn.aho@jhuapl.edu>
2025-08-04 15:39:33 -0400

    reorganization

--- scripts/fluid-sim.js
+++ scripts/fluid-sim.js
@@ -1050,7 +1050,7 @@ let bloomFramebuffers = [];
 let sunrays;
 let sunraysTemp;
 
-let ditheringTexture = createTextureAsync('LDR_LLL1_0.png');
+let ditheringTexture = createTextureAsync('media/img/LDR_LLL1_0.png');
 
 const blurProgram            = new Program(blurVertexShader, blurShader);
 const copyProgram            = new Program(baseVertexShader, copyShader);
